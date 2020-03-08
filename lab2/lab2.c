2719c3d959e1705b6d3bf92f75e9bd74fa563b27
REL <arielkohanim@gmail.com>
2020-03-07 18:42:39 -0800

    lab 2 initialized

---
+++ lab2/lab2.c
@@ -0,0 +1,19 @@
+// Ariel Kohanim 
+// 106776512
+// Comp 322 Lab 2
+
+#include <stdio.h>   /* printf, stderr, fprintf */
+#include <sys/types.h> /* pid_t */
+#include <unistd.h>  /* _exit, fork */
+#include <stdlib.h>  /* exit */
+#include <errno.h>   /* errno */
+
+
+int main(void)
+{
+pid_t  pid;
+
+
+
+return 0;
+}
