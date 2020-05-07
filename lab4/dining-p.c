1a5270c02a1bb271a8f0467c9861ad74537f8e9e
REL <arielkohanim@gmail.com>
2020-05-06 17:30:35 -0700

    Auto stash before rebase of "origin/master"
    did some more of part 1, added a struct to handle all philosopher related data

--- lab4/dining-p.c
+++ lab4/dining-p.c
@@ -13,24 +13,47 @@
 #include <signal.h>
 #include <sys/types.h>
 
+//create philosopher structure 
+struct Philosopher{
+    int seats;
+    int position;
+    int cycles;
+    char buff1[32];
+    char buff2[32];
+    sem_t* chopsticks[2];
+} philosopher;
 
 //globals 
 int counter = 0;
 
 void eat (phil_position){
 usleep(rand());
-printf("Philosopher #%d is eating \n", phil_position);
+printf("Philosopher #%d is eating \n", philosopher.position);
 }
 
 void think(phil_position){
 usleep(rand());
-printf("Philosopher #%d is thinking \n", phil_position)
+printf("Philosopher #%d is thinking \n", philosopher.position)
 }
 
+//part 1 step 3 
+void sigHandler(int sig)
+{
+    signal(SIGTERM, sigHandler);
+    if (sig == SIGTERM){
+        sem_destroy(philosopher)
+        sem_close(philosopher.chopsticks[0]);
+        sem_close(philosopher.chopsticks[1]);
+        sem_unlink(philosopher.buff1);
+        sem_unlink(philosopher.buff2);
+        
+        }
+    fprintf(stderr, "Philosopher [%d] completed %d cycles\n", philosopher.position, philosopher.cycles);
+}
 
 int main(int argc, char** argv){
 
-    int phil_position = atoi(argv[2]); //converts string arg to int 
+    int philosopher.position = atoi(argv[2]); //converts string arg to int 
     int i;
     int seats;
     
@@ -39,7 +62,6 @@ int main(int argc, char** argv){
         sem_wait(chopstick[i]);
         sem_wait(chopstick[(i + 1) % seats]);
 
-
         eat();
 
         sem_post(chopstick[i]);
@@ -47,7 +69,19 @@ int main(int argc, char** argv){
 
         think();
 
-        counter++;
+        counter++; 
     }
+    //part 1 step 3 
+    // signal(SIGTERM, handle_sigint); 
+    //     while (1) 
+    //     { 
+    //         printf("SIGTERM (%d) received...\n", handle_sigint);
+    //         int sem_destroy(sem_t *sem);
+    //         sem_destroy()
+
+    //     } 
+        
+
     return 0;
 }
+
