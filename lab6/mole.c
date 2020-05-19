12e970037e649618e83dad30019487accd7b4915
REL <arielkohanim@gmail.com>
2020-05-18 20:48:05 -0700

    Auto stash before rebase of "origin/master"
    worked on 6 needs lots of work

--- lab6/mole.c
+++ lab6/mole.c
@@ -1,6 +1,3 @@
-// Ariel Kohanim 
-// 106776512
-// Comp 322 Lab 6
 
 #include <stdio.h>
 #include <semaphore.h>
@@ -20,48 +17,6 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-
-
-void daemonize(const char *cmd){
-    int i, fd0, fd1, fd2;
-    pid_t;
-    struct rlimit rl;
-    struct sigaction sa;
-
-    //set the file creation mask to 0
-    umask(0); 
-
-    //fork and gave parent exit
-    if ((pid = fork())<0)
-    err_quit("%s: cant fotk",cmd);
-
-    else if (pid != 0){
-        exit(0);
-    }
-    //create a new session & create new proccess group 
-    setsid();
-
-    //change current working directiory to be "/"
-    if(chdir ("/") < 0){
-        err_quit("%s: can't change dir to /" cmd);
-    }
-
-    // close all open file descriptors
-    // need it to only close the UNNEEDED file descriptors 
-    // GETRLIMIT(2)    
-    if (rl.rlim_max == RLIM_INFINITY){
-        rl.rlim_max = 1024;
-    }
-    for(i=0; i<rl.rlim_max; i++){
-        close(i);
-    }
-
-    //reopen the std file descriptors to map to /dev/null
-    fd0= open("/dev/null",O_RDWR);
-    fd1 = dup(0);
-    fd2 = dup(0);
-
-}
 
 
 
