530fe7e1a43f208ab50faf4a1560da26cfdbc8c5
REL <arielkohanim@gmail.com>
2020-05-20 12:32:35 -0700

    Auto stash before rebase of "origin/master"
    lab 7

--- lab7/matrix_add.c
+++ lab7/matrix_add.c
@@ -11,6 +11,7 @@
 #include <time.h>
 #include <stdlib.h>
 
+
 void matrix_add (int block[][], int size, int scalar){
     for (i = 0; i < size; i++){
         for(j=0; j < size; j++){
@@ -19,10 +20,17 @@ void matrix_add (int block[][], int size, int scalar){
     }
 }
 
+
 int main(int argc, char **argv){
- int size;
- int block;
+ int size = atoi(argv[1];)
+ int block[][] = atoi(argv[2]);
  int scalar;
+
+ time_t start_time;
+
+//get current time
+ time(&start_time);
+ 
 
 
 
