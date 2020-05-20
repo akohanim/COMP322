a7484be4f2de89370aaa2df98f96fc6bc866fb85
REL <arielkohanim@gmail.com>
2020-05-20 12:56:44 -0700

    Auto stash before rebase of "origin/master"
    lab 7 layout

--- lab7/matrix_add.c
+++ lab7/matrix_add.c
@@ -10,6 +10,7 @@
 #include <unistd.h>
 #include <time.h>
 #include <stdlib.h>
+#include <aio.h>
 
 
 void matrix_add (int block[][], int size, int scalar){
@@ -22,17 +23,36 @@ void matrix_add (int block[][], int size, int scalar){
 
 
 int main(int argc, char **argv){
- int size = atoi(argv[1];)
- int block[][] = atoi(argv[2]);
- int scalar;
+    int size = atoi(argv[1];)
+    int blocks[][] = atoi(argv[2]);
+    int scalar;
 
- time_t start_time;
+    time_t start_time, end_time, total_time;
 
-//get current time
- time(&start_time);
- 
+    //get current time
+    time(&start_time);
+    
+    for (x = 1; x < blocks; x++){
+        for (y = 1; y < blocks; y++){
+
+            //asynch read request of matric [x,y]
+            aio_read (); //Enqueue a read request.  This is the asynchronous analog of read(2)
+
+            //block = async read return matrix[x, y]
+            block[x][y] = read(fd1, buf1, "a");
+
+    
+			matrix_add(block, block_size, scalar);
+
+		    //async write request block
+			aio_write(fd2, buf2, "a");
 
 
+        }
+    } 
+    time(&end_time);
+    total_time = end_time - start_time;
+    printf("total time = %ld\n", total_time);
 
 	return 0;
 }
